37a958461d5a739c2ad20d2dca4559eae12cee81
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2023-01-01 11:11:01 -0500

    feat: instaling phaser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './modules/intro/intro.component';
 import { PlatformMenuComponent } from './modules/platform-menu/platform-menu.component';
+import { PhaserTestComponent } from './modules/phaser-test/phaser-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
-    PlatformMenuComponent
+    PlatformMenuComponent,
+    PhaserTestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/phaser-test/phaser-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phaser-test',
+  templateUrl: './phaser-test.component.html',
+  styleUrls: ['./phaser-test.component.scss']
+})
+export class PhaserTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
